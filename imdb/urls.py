26dd35f1b041875d762ee7beaf864d9b7d22e270
amirali <3a_amirali@amiralis-MacBook-Pro.local>
2024-05-21 21:25:18 +0330

    remove useless comments

--- imdb/urls.py
+++ imdb/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import MovieViewSet
 
 router = DefaultRouter()
-router.register(r'movies', MovieViewSet, basename='movie') # todo: base name
+router.register(r'movies', MovieViewSet, basename='movie')
 
 urlpatterns = [
     path('', include(router.urls)),
